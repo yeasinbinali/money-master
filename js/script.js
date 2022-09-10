38ff3e514ffbd08194964a0155cada5042b2540e
yeasinbinali <yeasinbinali@gmail.com>
2022-09-10 22:59:54 +0600

    onclick added

--- js/script.js
+++ js/script.js
@@ -1,32 +1,51 @@
 function calculate(){
-    const foodInput = document.getElementById('food-input').value;
-    const rentInput = document.getElementById('rent-input').value;
-    const clothInput = document.getElementById('cloth-input').value;
-    const foodAmount = parseInt(foodInput);
-    const rentAmount = parseInt(rentInput);
-    const clothAmount = parseInt(clothInput);
+    const foodInput = document.getElementById('food-input');
+    const rentInput = document.getElementById('rent-input');
+    const clothInput = document.getElementById('cloth-input');
+    const foodValue = foodInput.value;
+    const rentValue = rentInput.value;
+    const clothValue = clothInput.value;
+
+    const foodAmount = parseInt(foodValue);
+    const rentAmount = parseInt(rentValue);
+    const clothAmount = parseInt(clothValue);
 
     const expensesAmount = foodAmount + rentAmount + clothAmount;
 
-    const totalExpenses = document.getElementById('total-expenses');
-    totalExpenses.innerText = expensesAmount;
+    const incomeInput = document.getElementById('income-input');
+    const incomeValue = incomeInput.value;
+    const incomeAmount = parseInt(incomeValue);
 
-    const incomeInput = document.getElementById('income-input').value;
-    const incomeAmount = parseInt(incomeInput);
+    if(incomeAmount > expensesAmount && incomeAmount > 0 && foodAmount >=0 && rentAmount >= 0 && clothAmount >= 0){
+        const totalExpenses = document.getElementById('total-expenses');
+        totalExpenses.innerText = expensesAmount;
 
-    const balanceAmount = document.getElementById('balance-amount');
-    balanceAmount.innerText = incomeAmount - foodAmount - rentAmount - clothAmount;
+        const balanceAmount = document.getElementById('balance-amount');
+        balanceAmount.innerText = incomeAmount - foodAmount - rentAmount - clothAmount;
+    }else{
+        foodInput.value = '';
+        rentInput.value = '';
+        clothInput.value = '';
+        incomeInput.value = '';
+    }
 }
 
 function save(){
-    const saveInput = document.getElementById('save-input').value;
-    const saveAmount = parseInt(saveInput);
+    const saveInput = document.getElementById('save-input');
+    const saveValue = saveInput.value;
+    const saveAmount = parseInt(saveValue);
 
     const balanceAmount = document.getElementById('balance-amount').innerText;
     const savingBalance = document.getElementById('saving-balance');
     const remainingAmount = document.getElementById('remaining-amount');
 
-    savingBalance.innerText = balanceAmount / saveAmount;
-
-    remainingAmount.innerText = balanceAmount - savingBalance.innerText;
+    if(saveAmount <= 100 && saveAmount > 0){
+        savingBalance.innerText = ((balanceAmount * saveAmount) / 100).toFixed(2);
+        remainingAmount.innerText = (balanceAmount - savingBalance.innerText).toFixed(2);
+        saveInput.value = '';
+    }else{
+        saveInput.value = '';
+        savingBalance.innerText = 0;
+        remainingAmount.innerText = 0;
+    }
 }
